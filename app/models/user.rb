0f9fbc8f91e6340e733588a3c2802a43fd637c14
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-11-09 22:20:19 +0000

    committing working devise logging in/out/register and table formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,28 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :role
+
 
   has_many :moves
   has_many :games_as_player1, class_name: "Game", foreign_key: :player1_id
   has_many :games_as_player2, class_name: "Game", foreign_key: :player2_id
-  
+
+  before_create :set_default_role
+
+
+  def role?(role_to_compare_to)
+   role_to_compare_to.to_s == self.role.try(:name).to_s
+ end
+
+ private
+ 
+ def set_default_role
+   self.role ||= Role.find_by_name('registered')
+ end
+ 
 end
+
