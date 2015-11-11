ef30d9249e6ab0241bb8a47b830882e0b9d26dbc
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-11-11 14:05:15 +0000

    committing seed data and styling to game list

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,9 @@
 ['registered', 'banned', 'moderator', 'admin'].each do |role|
   Role.find_or_create_by({name: role})
 end
+
+User.delete_all
+User.create!(email: "S.Osborne1992@gmail.com", username: "Osborne1992", first_name: "Simon", last_name: "Osborne", password: "password", password_confirmation: "password", role_id: 4)
+User.create!(email: "SRC_Osborne1992@hotmail.co.uk", username: "Ossy", first_name: "Simon", last_name: "Osborne", password: "password", password_confirmation: "password", role_id: 4)
+User.create!(email: "Osinaut@gmail.com", username: "Osinaut", first_name: "Simon", last_name: "Osborne", password: "password", password_confirmation: "password", role_id: 4)
+User.create!(email: "Osbot@simonosborne.co.uk", username: "SkyNET", first_name: "Skye", last_name: "Nethaniel", password: "password", password_confirmation: "password", role_id: 1)
